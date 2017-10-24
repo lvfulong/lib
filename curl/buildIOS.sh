52aed64a02a2e9d50165d67091ec97d27b7699ec
lvfulong <775331175@qq.com>
2017-10-24 16:50:30 +0800

    commit

--- curl/buildIOS.sh
+++ curl/buildIOS.sh
@@ -10,6 +10,36 @@ cd Contrib
 
 SDK_VERSION=$(xcodebuild -showsdks | grep iphoneos | sort | tail -n 1 | awk '{print substr($NF,9)}')
 
+rm -rf openssl
+mkdir openssl
+cd openssl
+tar xvzf ../../../OpenSSL-for-iPhone-master.zip
+#mv OpenSSL-for-iPhone-master openssl && touch openssl
+./OpenSSL-for-iPhone-master/build-libssl.sh --version=1.1.0f --deprecated --targets="ios-sim-cross-x86_64 ios-sim-cross-i386 ios64-cross-arm64 ios-cross-armv7"
+
+cd include
+rm -rf opensslconf.h
+echo "#ifdef __arm64__">>opensslconf.h
+echo "#include \"opensslconf_ios_arm64.h\"">> opensslconf.h
+echo "#elif __arm__">> opensslconf.h
+echo "#include  \"opensslconf_ios_armv7.h\"">> opensslconf.h
+echo "#elif __i386__">> opensslconf.h
+echo "#include  \"opensslconf_ios_i386.h\"">> opensslconf.h
+echo "#elif __x86_64__">> opensslconf.h
+echo "#include  \"opensslconf_ios_x86_64.h\"">> opensslconf.h
+echo "#else">> opensslconf.h
+echo "#error \"Unsupported architecture!\"">> opensslconf.h
+echo "#endif">> opensslconf.h
+
+cp ../bin/opensslconf_ios_armv7.h .
+cp ../bin/opensslconf_ios_arm64.h .
+cp ../bin/opensslconf_ios_i386.h .
+cp ../bin/opensslconf_ios_x86_64.h .
+
+cd ../..
+#cd ..
+
+
 ##############################################################################################
 rm -rf ios-armv7
 mkdir ios-armv7
@@ -43,25 +73,11 @@ cd ..
 
 
 
-#tar xvzf ../../openssl-OpenSSL_1_1_0f.tar.gz
-#mv openssl-OpenSSL_1_1_0f openssl && touch openssl
+cp ${top_dir}/Contrib/openssl/bin/iPhoneOS10.3-armv7.sdk/lib/libssl.a ${PREFIX}/lib
+cp ${top_dir}/Contrib/openssl/bin/iPhoneOS10.3-armv7.sdk/lib/libcrypto.a ${PREFIX}/lib
+mkdir ${PREFIX}/include/openssl
+cp  ${top_dir}/Contrib/openssl/include/* ${PREFIX}/include/openssl
 
-#cd openssl
-#export CC="xcrun clang"
-#export CXX="xcrun clang++"
-#export LD="xcrun ld"
-#export AR="xcrun ar"
-#export CCAS="gas-preprocessor.pl xcrun clang -c"
-#export RANLIB="xcrun ranlib"
-#export STRIP="xcrun strip"
-#export  PATH="${PREFIX}/bin:${PATH}"
-#export CPPFLAGS=" -isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=6.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CFLAGS=" -isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=6.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CXXFLAGS=" -isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=6.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export LDFLAGS=" -L${SDKROOT}/usr/lib -arch armv7 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
-#./Configure ios-cross --prefix=${PREFIX}  no-shared no-unit-test no-async
-#/Applications/Xcode.app/Contents/Developer/usr/bin/make install_sw
-#cd ..
 
 tar xvzf ../../curl-7.56.0.tar.gz
 mv curl-7.56.0 curl && touch curl
@@ -82,7 +98,7 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=6.0 -mcp
 export CXXFLAGS=" -isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=6.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
 export LDFLAGS=" -L${SDKROOT}/usr/lib -arch armv7 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
 ./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="arm-apple-darwin" --target="arm-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic \
---with-darwinssl \
+--with-ssl=${PREFIX} \
 --with-zlib \
 --enable-ipv6 \
 --disable-ldap
@@ -121,26 +137,11 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=6.0  -I$
 cd ..
 
 
-#tar xvzf ../../openssl-OpenSSL_1_1_0f.tar.gz
-#mv openssl-OpenSSL_1_1_0f openssl && touch openssl
+cp ${top_dir}/Contrib/openssl/bin/iPhoneOS10.3-arm64.sdk/lib/libssl.a ${PREFIX}/lib
+cp ${top_dir}/Contrib/openssl/bin/iPhoneOS10.3-arm64.sdk/lib/libcrypto.a ${PREFIX}/lib
+mkdir ${PREFIX}/include/openssl
+cp  ${top_dir}/Contrib/openssl/include/* ${PREFIX}/include/openssl
 
-#cd openssl
-#export CC="xcrun clang"
-#export CXX="xcrun clang++"
-#export LD="xcrun ld"
-#export AR="xcrun ar"
-#export CCAS="gas-preprocessor.pl xcrun clang -c"
-#export RANLIB="xcrun ranlib"
-#export STRIP="xcrun strip"
-#export PATH="${PREFIX}/bin:${PATH}"
-#export CPPFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CXXFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export LDFLAGS=" -L${SDKROOT}/usr/lib -arch arm64 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
-#./Configure ios64-cross --prefix=${PREFIX}  no-shared no-unit-test no-async
-
-#/Applications/Xcode.app/Contents/Developer/usr/bin/make install_sw
-#cd ..
 
 tar xvzf ../../curl-7.56.0.tar.gz
 mv curl-7.56.0 curl && touch curl
@@ -161,7 +162,7 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=6.0  -I$
 export CXXFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
 export LDFLAGS=" -L${SDKROOT}/usr/lib -arch arm64 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
 ./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="arm-apple-darwin" --target="arm-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic \
---with-darwinssl \
+--with-ssl=${PREFIX} \
 --with-zlib \
 --enable-ipv6 \
 --disable-ldap
@@ -198,26 +199,11 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=6.0  -I${
 cd ..
 
 
-#tar xvzf ../../openssl-OpenSSL_1_1_0f.tar.gz
-#mv openssl-OpenSSL_1_1_0f openssl && touch openssl
+cp ${top_dir}/Contrib/openssl/bin/iPhoneSimulator10.3-i386.sdk/lib/libssl.a ${PREFIX}/lib
+cp ${top_dir}/Contrib/openssl/bin/iPhoneSimulator10.3-i386.sdk/lib/libcrypto.a ${PREFIX}/lib
+mkdir ${PREFIX}/include/openssl
+cp  ${top_dir}/Contrib/openssl/include/* ${PREFIX}/include/openssl
 
-#cd openssl
-#export CC="xcrun clang"
-#export CXX="xcrun clang++"
-#export LD="xcrun ld"
-#export AR="xcrun ar"
-#export CCAS="gas-preprocessor.pl xcrun clang -c"
-#export RANLIB="xcrun ranlib"
-#export STRIP="xcrun strip"
-#export PATH="${PREFIX}/bin:${PATH}"
-#export CPPFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CXXFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export LDFLAGS=" -L${SDKROOT}/usr/lib -arch i386 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
-#./Configure ios-sim-cross-i386 --prefix=${PREFIX}  no-shared no-unit-test no-async
-
-#/Applications/Xcode.app/Contents/Developer/usr/bin/make install_sw
-#cd ..
 
 tar xvzf ../../curl-7.56.0.tar.gz
 mv curl-7.56.0 curl && touch curl
@@ -238,7 +224,7 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=6.0  -I${
 export CXXFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
 export LDFLAGS=" -L${SDKROOT}/usr/lib -arch i386 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
 ./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="i386-apple-darwin" --target="i386-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic \
---with-darwinssl \
+--with-ssl=${PREFIX} \
 --with-zlib \
 --enable-ipv6 \
 --disable-ldap
@@ -276,26 +262,12 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=6.0  -I
 cd ..
 
 
-#tar xvzf ../../openssl-OpenSSL_1_1_0f.tar.gz
-#mv openssl-OpenSSL_1_1_0f openssl && touch openssl
+cp ${top_dir}/Contrib/openssl/bin/iPhoneSimulator10.3-x86_64.sdk/lib/libssl.a ${PREFIX}/lib
+cp ${top_dir}/Contrib/openssl/bin/iPhoneSimulator10.3-x86_64.sdk/lib/libcrypto.a ${PREFIX}/lib
+mkdir ${PREFIX}/include/openssl
+cp  ${top_dir}/Contrib/openssl/include/* ${PREFIX}/include/openssl
 
-#cd openssl
-#export CC="xcrun clang"
-#export CXX="xcrun clang++"
-#export LD="xcrun ld"
-#export AR="xcrun ar"
-#export CCAS="gas-preprocessor.pl xcrun clang -c"
-#export RANLIB="xcrun ranlib"
-#export STRIP="xcrun strip"
-#export PATH="${PREFIX}/bin:${PATH}"
-#export CPPFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export CXXFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
-#export LDFLAGS=" -L${SDKROOT}/usr/lib -arch x86_64 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
-#./Configure ios-sim-cross-x86_64 --prefix=${PREFIX}  no-shared no-unit-test no-async
 
-#/Applications/Xcode.app/Contents/Developer/usr/bin/make install_sw
-#cd ..
 
 tar xvzf ../../curl-7.56.0.tar.gz
 mv curl-7.56.0 curl && touch curl
@@ -316,7 +288,7 @@ export CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=6.0  -I
 export CXXFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=6.0  -I${PREFIX}/include -O3 -DNDEBUG -fPIC"
 export LDFLAGS=" -L${SDKROOT}/usr/lib -arch x86_64 -isysroot ${SDKROOT} -miphoneos-version-min=6.0 -L${PREFIX}/lib"
 ./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="x86_64-apple-darwin" --target="x86_64-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic \
---with-darwinssl \
+--with-ssl=${PREFIX} \
 --with-zlib \
 --enable-ipv6 \
 --disable-ldap
@@ -326,5 +298,5 @@ cd ../..
 ##############################################################################################
 lipo -create install-ios/armv7/lib/libz.a install-ios/arm64/lib/libz.a install-ios/x86_64/lib/libz.a install-ios/i386/lib/libz.a -output install-ios/libz.a
 lipo -create install-ios/armv7/lib/libcurl.a install-ios/arm64/lib/libcurl.a install-ios/x86_64/lib/libcurl.a install-ios/i386/lib/libcurl.a -output install-ios/libcurl.a
-#lipo -create install-ios/armv7/lib/libssl.a install-ios/arm64/lib/libssl.a install-ios/x86_64/lib/libssl.a install-ios/i386/lib/libssl.a -output install-ios/libssl.a
-#lipo -create install-ios/armv7/lib/libcrypto.a install-ios/arm64/lib/libcrypto.a install-ios/x86_64/lib/libcrypto.a install-ios/i386/lib/libcrypto.a -output install-ios/libcrypto.a
+lipo -create install-ios/armv7/lib/libssl.a install-ios/arm64/lib/libssl.a install-ios/x86_64/lib/libssl.a install-ios/i386/lib/libssl.a -output install-ios/libssl.a
+lipo -create install-ios/armv7/lib/libcrypto.a install-ios/arm64/lib/libcrypto.a install-ios/x86_64/lib/libcrypto.a install-ios/i386/lib/libcrypto.a -output install-ios/libcrypto.a
