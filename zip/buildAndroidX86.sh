21f4073fa8a66f9d690933fc8e8d4b8c669a8898
lvfulong <775331175@qq.com>
2017-10-25 19:08:22 +0800

    commit

--- zip/buildAndroidX86.sh
+++ zip/buildAndroidX86.sh
@@ -5,7 +5,7 @@ PATH="${current_dir}/..:${PATH}"
 mkdir Contrib
 cd Contrib
 
-export ANDROID_NDK=/Users/layabox_mac/Music/lib/android-ndk-r12b
+export ANDROID_NDK=/Users/layabox_mac/Public/lib/android-ndk-r10e
 PATH="${ANDROID_NDK}/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin:${PATH}"
 ##############################################################################################
 rm -rf android-x86
