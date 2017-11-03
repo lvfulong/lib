ab7decb71a1f28d2e64ef11ccadfa2dad96a2e83
lvfulong <775331175@qq.com>
2017-11-03 22:04:17 +0800

    commit

--- curl/buildAndroidArmv7.sh
+++ curl/buildAndroidArmv7.sh
@@ -4,7 +4,7 @@ PATH="${current_dir}/..:${PATH}"
 
 mkdir Contrib
 cd Contrib
-export ANDROID_NDK=/Users/layabox_mac/Music/lib/android-ndk-r12b
+export ANDROID_NDK=/Users/layabox_mac/Music/lib/android-ndk-r10e
 PATH="${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin:${PATH}"
 ##############################################################################################
 rm -rf android-armv7
