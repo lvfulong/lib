70d9560595cb196c9760b15e3a136e877e1aef3a
lvfulong <775331175@qq.com>
2019-08-14 20:23:51 +0800

    c

--- zlib/buildIOS.sh
+++ zlib/buildIOS.sh
@@ -37,33 +37,9 @@ export  CXXFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0   
 export  LDFLAGS="-L${SDKROOT}/usr/lib -arch armv7 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
 export  CHOST=arm-apple-darwin
 export  CFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0   -mcpu=cortex-a8 -I${PREFIX}/include -03 -DNDEBUG"
-./configure --prefix=${PREFIX} --static
+./configure --prefix=${PREFIX} --static --zprefix
 /Applications/Xcode.app/Contents/Developer/usr/bin/make install
 
-
-cd .. 
-tar xvzf ../../libpng-1.6.34.tar.xz
-mv libpng-1.6.34 png && touch png
-
-
-mkdir -p -- ${PREFIX}/share/aclocal && cd png && autoreconf -fiv -I${PREFIX}/share/aclocal
-
-
-export  CC="xcrun clang"
-export  CXX="xcrun clang++" 
-export  LD="xcrun ld" 
-export  AR="xcrun ar" 
-export  CCAS="gas-preprocessor.pl xcrun clang -c" 
-export  RANLIB="xcrun ranlib" 
-export  STRIP="xcrun strip" 
-export  PATH="${PREFIX}/bin:${PATH}" 
-export  CPPFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG"
-export  CFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG"
-export  CXXFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0 -mcpu=cortex-a8  -I${PREFIX}/include -O3 -DNDEBUG"
-export  LDFLAGS="-L${SDKROOT}/usr/lib -arch armv7 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
-./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="arm-apple-darwin" --target="arm-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic
-
-/Applications/Xcode.app/Contents/Developer/usr/bin/make install
 cd ../..
 
 ##############################################################################################
@@ -91,32 +67,10 @@ export  CXXFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  
 export  LDFLAGS=" -L${SDKROOT}/usr/lib -arch arm64 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
 export  CHOST=arm-apple-darwin 
 export  CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG "
-./configure --prefix=${PREFIX} --static
+./configure --prefix=${PREFIX} --static --zprefix
 
 /Applications/Xcode.app/Contents/Developer/usr/bin/make install
 
-cd .. 
-tar xvzf ../../libpng-1.6.34.tar.xz
-mv libpng-1.6.34 png && touch png
-
-mkdir -p -- ${PREFIX}/share/aclocal && cd png && autoreconf -fiv -I${PREFIX}/share/aclocal
-
-export  CC="xcrun clang" 
-export  CXX="xcrun clang++" 
-export  LD="xcrun ld" 
-export  AR="xcrun ar" 
-export  CCAS="gas-preprocessor.pl xcrun clang -c" 
-export  RANLIB="xcrun ranlib" 
-export  STRIP="xcrun strip" 
-export  PATH="${PREFIX}/bin:${PATH}" 
-export  CPPFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export  CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export  CXXFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export  LDFLAGS=" -L${SDKROOT}/usr/lib -arch arm64 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
-./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="arm-apple-darwin" --target="arm-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic
-
-
-/Applications/Xcode.app/Contents/Developer/usr/bin/make install
 cd ../..
 ##############################################################################################
 rm -rf ios-i386
@@ -144,30 +98,10 @@ export  CXXFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -
 export  LDFLAGS=" -L${SDKROOT}/usr/lib -arch i386 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
 export   CHOST=i386-apple-darwin 
 export  CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG "
-./configure --prefix=${PREFIX} --static
+./configure --prefix=${PREFIX} --static --zprefix
 /Applications/Xcode.app/Contents/Developer/usr/bin/make install
 
-cd .. 
-tar xvzf ../../libpng-1.6.34.tar.xz
-mv libpng-1.6.34 png && touch png
 
-mkdir -p -- ${PREFIX}/share/aclocal && cd png && autoreconf -fiv -I${PREFIX}/share/aclocal
-
-export  CC="xcrun clang" 
-export  CXX="xcrun clang++" 
-export  LD="xcrun ld" 
-export  AR="xcrun ar" 
-export  CAS="gas-preprocessor.pl xcrun clang -c" 
-export  RANLIB="xcrun ranlib" 
-export  STRIP="xcrun strip" 
-export  PATH="${PREFIX}/bin:${PATH}"  
-export  CPPFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export  CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export  CXXFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export  LDFLAGS=" -L${SDKROOT}/usr/lib -arch i386 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
-./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="i386-apple-darwin" --target="i386-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic
-
-/Applications/Xcode.app/Contents/Developer/usr/bin/make install
 cd ../..
 ##############################################################################################
 rm -rf ios-x86_64
@@ -194,33 +128,11 @@ export CXXFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  
 export LDFLAGS=" -L${SDKROOT}/usr/lib -arch x86_64 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
 export CHOST=x86_64-apple-darwin export
 CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG "
-./configure --prefix=${PREFIX} --static
+./configure --prefix=${PREFIX} --static --zprefix
 
 
 /Applications/Xcode.app/Contents/Developer/usr/bin/make install
 
-cd ..
-tar xvzf ../../libpng-1.6.34.tar.xz
-mv libpng-1.6.34 png && touch png
-
-mkdir -p -- ${PREFIX}/share/aclocal && cd png && autoreconf -fiv -I${PREFIX}/share/aclocal
-
-export CC="xcrun clang"
-export CXX="xcrun clang++"
-export LD="xcrun ld"
-export AR="xcrun ar"
-export CCAS="gas-preprocessor.pl xcrun clang -c"
-export RANLIB="xcrun ranlib" STRIP="xcrun strip"
-export  PATH="${PREFIX}/bin:${PATH}"
-export CPPFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export CXXFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
-export LDFLAGS=" -L${SDKROOT}/usr/lib -arch x86_64 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
-./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="x86_64-apple-darwin" --target="x86_64-apple-darwin" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking --with-pic
-
-
-/Applications/Xcode.app/Contents/Developer/usr/bin/make install
 cd ../..
 ##############################################################################################
-lipo -create install-ios/armv7/lib/libpng16.a install-ios/arm64/lib/libpng16.a install-ios/x86_64/lib/libpng16.a install-ios/i386/lib/libpng16.a -output install-ios/libpng.a
 lipo -create install-ios/armv7/lib/libz.a install-ios/arm64/lib/libz.a install-ios/x86_64/lib/libz.a install-ios/i386/lib/libz.a -output install-ios/libz.a
