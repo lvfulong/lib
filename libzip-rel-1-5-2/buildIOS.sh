6f8073db951a56ffb15e7842cd9b2f1e9d08017b
lvfulong <775331175@qq.com>
2019-08-14 21:50:23 +0800

    C

--- libzip-rel-1-5-2/buildIOS.sh
+++ libzip-rel-1-5-2/buildIOS.sh
@@ -19,6 +19,28 @@ PREFIX="${top_dir}/contrib/install-ios/armv7"
 IOS_PLATFORM=OS
 SDKROOT=`xcode-select -print-path`/Platforms/iPhone${IOS_PLATFORM}.platform/Developer/SDKs/iPhone${IOS_PLATFORM}${SDK_VERSION}.sdk
 
+tar xvzf ../../zlib-1.2.8.tar.gz
+mv zlib-1.2.8 zlib && touch zlib
+cd zlib
+export  CC="xcrun clang"
+export  CXX="xcrun clang++"
+export  LD="xcrun ld"
+export  AR="xcrun ar"
+export  CCAS="gas-preprocessor.pl xcrun clang -c"
+export  RANLIB="xcrun ranlib" 
+export  STRIP="xcrun strip"
+export  PATH="${PREFIX}/bin:${PATH}" 
+export  CPPFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0   -mcpu=cortex-a8 -I${PREFIX}/include -O3 -DNDEBUG"
+export  CFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0   -mcpu=cortex-a8 -I${PREFIX}/include -O3 -DNDEBUG"
+export  CXXFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0   -mcpu=cortex-a8 -I${PREFIX}/include -O3 -DNDEBUG"
+export  LDFLAGS="-L${SDKROOT}/usr/lib -arch armv7 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
+export  CHOST=arm-apple-darwin
+export  CFLAGS="-isysroot ${SDKROOT} -arch armv7 -miphoneos-version-min=8.0   -mcpu=cortex-a8 -I${PREFIX}/include -O3 -DNDEBUG"
+./configure --prefix=${PREFIX} --static --zprefix
+/Applications/Xcode.app/Contents/Developer/usr/bin/make install
+
+cd ..
+
 CMAKE_TOOLCHAIN_PATH=`pwd`
 rm -f toolchain.cmake
 echo "set(CMAKE_SYSTEM_NAME Darwin)" >> toolchain.cmake
@@ -64,6 +86,29 @@ cd ios-arm64
 PREFIX="${top_dir}/contrib/install-ios/arm64"
 IOS_PLATFORM=OS
 SDKROOT=`xcode-select -print-path`/Platforms/iPhone${IOS_PLATFORM}.platform/Developer/SDKs/iPhone${IOS_PLATFORM}${SDK_VERSION}.sdk
+
+tar xvzf ../../zlib-1.2.8.tar.gz
+mv zlib-1.2.8 zlib && touch zlib
+
+cd zlib
+export  CC="xcrun clang"
+export  CXX="xcrun clang++" 
+export  LD="xcrun ld" 
+export  AR="xcrun ar" 
+export  CCAS="gas-preprocessor.pl xcrun clang -c" 
+export  RANLIB="xcrun ranlib" STRIP="xcrun strip" 
+export  PATH="${PREFIX}/bin:${PATH}" 
+export  CPPFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export  CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export  CXXFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export  LDFLAGS=" -L${SDKROOT}/usr/lib -arch arm64 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
+export  CHOST=arm-apple-darwin 
+export  CFLAGS=" -isysroot ${SDKROOT} -arch arm64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG "
+./configure --prefix=${PREFIX} --static --zprefix
+
+/Applications/Xcode.app/Contents/Developer/usr/bin/make install
+
+cd ..
 
 CMAKE_TOOLCHAIN_PATH=`pwd`
 rm -f toolchain.cmake
@@ -111,6 +156,30 @@ PREFIX="${top_dir}/contrib/install-ios/i386"
 IOS_PLATFORM=Simulator
 SDKROOT=`xcode-select -print-path`/Platforms/iPhone${IOS_PLATFORM}.platform/Developer/SDKs/iPhone${IOS_PLATFORM}${SDK_VERSION}.sdk
 
+tar xvzf ../../zlib-1.2.8.tar.gz
+mv zlib-1.2.8 zlib && touch zlib
+
+cd zlib
+export  CC="xcrun clang" 
+export  CXX="xcrun clang++" 
+export  LD="xcrun ld" 
+export  AR="xcrun ar" 
+export  CCAS="gas-preprocessor.pl xcrun clang -c" 
+export  RANLIB="xcrun ranlib" 
+export  STRIP="xcrun strip" 
+export  PATH="${PREFIX}/bin:${PATH}" 
+export  CPPFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export  CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export  CXXFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export  LDFLAGS=" -L${SDKROOT}/usr/lib -arch i386 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
+export   CHOST=i386-apple-darwin 
+export  CFLAGS=" -isysroot ${SDKROOT} -arch i386 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG "
+./configure --prefix=${PREFIX} --static --zprefix
+/Applications/Xcode.app/Contents/Developer/usr/bin/make install
+
+
+cd ..
+
 CMAKE_TOOLCHAIN_PATH=`pwd`
 rm -f toolchain.cmake
 echo "set(CMAKE_SYSTEM_NAME Darwin)" >> toolchain.cmake
@@ -155,6 +224,30 @@ cd ios-x86_64
 PREFIX="${top_dir}/contrib/install-ios/x86_64"
 IOS_PLATFORM=Simulator
 SDKROOT=`xcode-select -print-path`/Platforms/iPhone${IOS_PLATFORM}.platform/Developer/SDKs/iPhone${IOS_PLATFORM}${SDK_VERSION}.sdk
+
+tar xvzf ../../zlib-1.2.8.tar.gz
+mv zlib-1.2.8 zlib && touch zlib
+
+cd zlib
+export CC="xcrun clang"
+export CXX="xcrun clang++"
+export LD="xcrun ld"
+export AR="xcrun ar"
+export CCAS="gas-preprocessor.pl xcrun clang -c"
+export RANLIB="xcrun ranlib" STRIP="xcrun strip"
+export  PATH="${PREFIX}/bin:${PATH}"
+export CPPFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export CXXFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG"
+export LDFLAGS=" -L${SDKROOT}/usr/lib -arch x86_64 -isysroot ${SDKROOT} -miphoneos-version-min=8.0 -L${PREFIX}/lib"
+export CHOST=x86_64-apple-darwin export
+CFLAGS=" -isysroot ${SDKROOT} -arch x86_64 -miphoneos-version-min=8.0  -I${PREFIX}/include -O3 -DNDEBUG "
+./configure --prefix=${PREFIX} --static --zprefix
+
+
+/Applications/Xcode.app/Contents/Developer/usr/bin/make install
+
+cd ..
 
 CMAKE_TOOLCHAIN_PATH=`pwd`
 rm -f toolchain.cmake
