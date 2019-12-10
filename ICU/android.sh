510d5a1fd454a2a271a30d0d127f7191ba635e2b
lvfulong <775331175@qq.com>
2019-12-10 18:17:20 +0800

    c

--- ICU/android.sh
+++ ICU/android.sh
@@ -46,3 +46,72 @@ $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
 	
 	
 	gnumake install
+	
+cd ..
+mkdir x86
+cd x86
+
+$ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
+    --platform=android-15 \
+    --install-dir=$ANDROID_TOOLCHAIN \
+    --toolchain=x86-4.9 \
+    --llvm-version=3.6
+
+../source/configure --prefix=$(pwd)/prebuilt \
+    --host=i686-linux-android \
+    --enable-static \
+    --enable-shared=no \
+    --enable-extras=no \
+    --enable-strict=no \
+    --enable-icuio=no \
+    --enable-layout=no \
+    --enable-layoutex=no \
+    --enable-tools=no \
+    --enable-tests=no \
+    --enable-samples=no \
+    --enable-dyload=no \
+    -with-cross-build=$CROSS_BUILD_DIR \
+    CFLAGS='-Os -march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32' \
+    CXXFLAGS='--std=c++11 -march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32' \
+    CC=i686-linux-android-clang \
+    CXX=i686-linux-android-clang++ \
+    AR=i686-linux-android-ar \
+    RINLIB=i686-linux-android-ranlib \
+    --with-data-packaging=static
+
+gnumake install
+
+
+
+cd ..
+mkdir aarch64
+cd aarch64
+
+$ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
+    --platform=android-21 \
+    --install-dir=$ANDROID_TOOLCHAIN \
+    --toolchain=aarch64-linux-android-clang3.6
+
+../source/configure --prefix=$(pwd)/prebuilt \
+    --host=aarch64-linux-android \
+    --enable-static \
+    --enable-shared=no \
+    --enable-extras=no \
+    --enable-strict=no \
+    --enable-icuio=no \
+    --enable-layout=no \
+    --enable-layoutex=no \
+    --enable-tools=no \
+    --enable-tests=no \
+    --enable-samples=no \
+    --enable-dyload=no \
+    -with-cross-build=$CROSS_BUILD_DIR \
+    CFLAGS='-Os' \
+    CXXFLAGS='--std=c++11' \
+    CC=aarch64-linux-android-clang \
+    CXX=aarch64-linux-android-clang++ \
+    AR=aarch64-linux-android-ar \
+    RINLIB=aarch64-linux-android-ranlib \
+    --with-data-packaging=static
+
+gnumake install
