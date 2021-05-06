3a2f5896f275e0b6754891581084c7dc56d80df4
lvfulong <775331175@qq.com>
2021-05-06 14:23:07 +0800

    cc

--- FFmpeg/buildAndroidArmv7.sh
+++ FFmpeg/buildAndroidArmv7.sh
@@ -2,13 +2,9 @@
 current_dir=`pwd`
 top_dir=$current_dir
 #PATH="${current_dir}/..:${PATH}"
-export API=16
 mkdir Contrib
 cd Contrib
 
-export ANDROID_NDK=/Users/layabox_mac/Public/lib/android-ndk-r10e
-TOOLCHAIN=darwin-x86_64
- export PATH=$PATH:${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${TOOLCHAIN}/bin
 ##############################################################################################
 rm -rf android-armv7
 mkdir android-armv7
@@ -21,97 +17,50 @@ tar xvzf ../../FFmpeg-n3.0.zip
 mv FFmpeg-n3.0 FFmpeg && touch FFmpeg
 cd FFmpeg
 
-HOST="arm-linux-androideabi"
-ANDROID_SYSROOT="${ANDROID_NDK}"/toolchains/llvm/prebuilt/"${TOOLCHAIN}"/sysroot
-HOST_PKG_CONFIG_PATH=$(command -v pkg-config)
-if [ -z "${HOST_PKG_CONFIG_PATH}" ]; then
-  echo -e "\n(*) pkg-config command not found\n"
-  exit 1
-fi
+# 修改为自己NDK包根目录
+export NDK_HOME=/Users/LayaBox/lvfulong/lib/android-ndk-r10e
+#根据自己的需求修改编译平台版本
+export PLATFORM_VERSION=android-14
+#定义编译方法
+function build
+{
+    #echo 输出命令
+    echo "start build ffmpeg for $ARCH"
+    #调用configure命令开始编译，并传入对应的参数
+    ./configure --target-os=linux \
+    --prefix=$PREFIX --arch=$ARCH \
+    --disable-doc \
+    --disable-static \
+    --disable-yasm \
+    --disable-asm \
+    --disable-symver \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --cross-prefix=$CROSS_COMPILE \
+    --enable-cross-compile \
+    --enable-shared \
+    --enable-gpl \
+    --sysroot=$SYSROOT \
+    --enable-small \
+    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+    make clean
+    make
+    make install
+    echo "build ffmpeg for $ARCH finished"
+}
 
-  TARGET_CPU="armv7-a"
-  TARGET_ARCH="armv7-a"
-  ASM_OPTIONS=" --disable-neon --enable-asm --enable-inline-asm"
-  # ALWAYS BUILD SHARED LIBRARIES
-BUILD_LIBRARY_OPTIONS="--disable-static --enable-shared"
-
-# OPTIMIZE FOR SPEED INSTEAD OF SIZE
-#if [[ -z ${FFMPEG_KIT_OPTIMIZED_FOR_SPEED} ]]; then
-#  SIZE_OPTIONS="--enable-small"
-#else
-  SIZE_OPTIONS=""
-#fi
-
-
-# SET DEBUG OPTIONS
-#if [[ -z ${FFMPEG_KIT_DEBUG} ]]; then
-
-  # SET LTO FLAGS
-#  if [[ -z ${NO_LINK_TIME_OPTIMIZATION} ]]; then
-#    DEBUG_OPTIONS="--disable-debug --enable-lto"
-#  else
-    DEBUG_OPTIONS="--disable-debug --disable-lto"
- # fi
-#else
-#  DEBUG_OPTIONS="--enable-debug --disable-stripping"
-#fi
-
-  export AR=${HOST}-ar
-  export CC="armv7a-linux-androideabi${API}-clang"
-  export CXX="armv7a-linux-androideabi${API}-clang++"
-
-  export LD=${HOST}-ld
-  export RANLIB=${HOST}-ranlib
-  export STRIP=${HOST}-strip
-  export NM=${HOST}-nm
-  
-./configure \
-  --cross-prefix="${HOST}-" \
-  --sysroot="${ANDROID_SYSROOT}" \
-  --prefix="${PREFIX}" \
-  --pkg-config="${HOST_PKG_CONFIG_PATH}" \
-  --enable-version3 \
-  --arch="${TARGET_ARCH}" \
-  --cpu="${TARGET_CPU}" \
-  --cc="${CC}" \
-  --cxx="${CXX}" \
-  --ranlib="${RANLIB}" \
-  --strip="${STRIP}" \
-  --nm="${NM}" \
-  --extra-libs="$(pkg-config --libs --static cpu-features)" \
-  --target-os=android \
-  ${ASM_OPTIONS} \
-  --enable-cross-compile \
-  --enable-pic \
-  --enable-optimizations \
-  --enable-swscale \
-  ${BUILD_LIBRARY_OPTIONS} \
-  --disable-outdev=fbdev \
-  --disable-indev=fbdev \
-  ${SIZE_OPTIONS} \
-  --disable-openssl \
-  --disable-xmm-clobber-test \
-  ${DEBUG_OPTIONS} \
-  --disable-neon-clobber-test \
-  --disable-programs \
-  --disable-postproc \
-  --disable-doc \
-  --disable-htmlpages \
-  --disable-manpages \
-  --disable-podpages \
-  --disable-txtpages \
-  --disable-schannel \
-  --disable-securetransport \
-  --disable-xlib \
-  --disable-nvenc \
-  --disable-vaapi \
-  --disable-vdpau \
-  --disable-videotoolbox \
-  --disable-nvenc \
-  --disable-vaapi \
-  --disable-vdpau 
-
-/Applications/Xcode.app/Contents/Developer/usr/bin/make install
-cd ../..
-
-##############################################################################################
+#编译 arm-v7a
+PLATFORM_VERSION=android-21
+ARCH=arm
+CPU=armeabi-v7a #CPU架构
+PREFIX=$(pwd)/android_all/$CPU  #输出路径：当前目录/android_all/CPU架构/
+TOOLCHAIN=$NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-androideabi- #交叉编译环境路径
+ADDI_CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb -mfpu=neon"
+ADDI_LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
+SYSROOT=$NDK_HOME/platforms/$PLATFORM_VERSION/arch-$ARCH/
+build
