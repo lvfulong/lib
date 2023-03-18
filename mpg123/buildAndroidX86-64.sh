86532049a7fb9a5b2375d50463c612b90f1ebfce
helloworldlv <775331175@qq.com>
2023-03-18 19:31:54 +0800

    x86-64

---
+++ mpg123/buildAndroidX86-64.sh
@@ -0,0 +1,39 @@
+current_dir=`pwd`
+top_dir=$current_dir
+PATH="${current_dir}/..:${PATH}"
+
+mkdir Contrib
+cd Contrib
+
+export ANDROID_NDK=/Users/LayaBox/lvfulong/lib/android-ndk-r10e
+PATH="${ANDROID_NDK}/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/bin:${PATH}"
+##############################################################################################
+rm -rf android-x86_64
+mkdir android-x86_64
+cd android-x86_64
+
+PREFIX="${top_dir}/contrib/install-android/x86_64"
+SDKROOT="${ANDROID_NDK}/platforms/android-21/arch-x86_64"
+
+tar xvzf ../../mpg123-1.26.2.tar.bz2
+mv mpg123-1.26.2 mpg123 && touch mpg123
+cd mpg123
+
+export CC="x86_64-linux-android-gcc --sysroot=${SDKROOT}"
+export CXX="x86_64-linux-android-g++ --sysroot=${SDKROOT}"
+export LD="x86_64-linux-android-ld"
+export AR="x86_64-linux-android-ar"
+export CCAS="x86_64-linux-android-gcc --sysroot=${SDKROOT} -c"
+export RANLIB="x86_64-linux-android-ranlib"
+export STRIP="x86_64-linux-android-strip"
+export PATH="${PREFIX}/bin:${PATH}"
+export CPPFLAGS="-fPIC -ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat  -I${PREFIX}/include -O1 -DNDEBUG"
+export CFLAGS="-fPIC -ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat  -I${PREFIX}/include -O1 -DNDEBUG"
+export CXXFLAGS="-fPIC -ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat  -I${PREFIX}/include -O1 -DNDEBUG"
+export LDFLAGS=" -L${PREFIX}/lib"
+./configure --prefix=${PREFIX} --datarootdir="${PREFIX}/share" --includedir="${PREFIX}/include" --libdir="${PREFIX}/lib" --build="x86_64-apple-darwin14" --host="x86_64-linux-android" --target="x86_64-linux-android" --program-prefix="" --enable-static --disable-shared --disable-dependency-tracking
+
+/Applications/Xcode.app/Contents/Developer/usr/bin/make install
+cd ../..
+
+##############################################################################################
