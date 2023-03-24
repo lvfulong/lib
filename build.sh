826672244999dc83fe9ba7032597ee4302590250
helloworldlv <775331175@qq.com>
2023-03-24 21:19:15 +0800

    freetype

--- build.sh
+++ build.sh
@@ -534,6 +534,115 @@ function build_zip {
 	rm -rf ${root_dir}/${lib_name}/${lib_source_dir}
 	cd ${root_dir}
 }
+
+function build_freetype {
+	local build_type=$1
+    local arch=$2
+    local platform=$3
+	#depends zlib png
+	build_zlib ${build_type} ${arch} ${platform}
+	build_png ${build_type} ${arch} ${platform}
+	local lib_name=freetype
+	local build_dir_root="${root_dir}/build/${platform}-${build_type}-${arch}"
+    local build_dir="${build_dir_root}/${lib_name}"
+	mkdir -p "${build_dir}"
+	cd ${lib_name}
+	local lib_source_dir=freetype-2.13.0
+	rm -rf ${lib_source_dir}
+	tar xvzf ${lib_source_dir}.tar.gz
+
+	cd ..
+	cd ${build_dir}
+	
+
+	#-DPLATFORM_NAME="${platform}"
+	#-DCMAKE_BUILD_TYPE=${build_type} 
+	if [[ "$3" == "windows" ]]; then	
+	
+		local generator="Visual Studio 14 2015"
+		if [[ "$2" == "win64" ]]; then
+			generator="Visual Studio 14 2015 Win64"
+		fi
+		cmake -G "${generator}" \
+			-DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+			-DCMAKE_PREFIX_PATH=${build_dir_root} \
+			-DFT_REQUIRE_ZLIB=TRUE \
+			-DFT_REQUIRE_BZIP2=FALSE \
+			-DFT_REQUIRE_PNG=TRUE \
+			-DFT_REQUIRE_HARFBUZZ=FALSE \
+			-DFT_REQUIRE_BROTLI=FALSE \
+			../../../${lib_name}/${lib_source_dir}
+	
+		cmake --build . --config ${build_type} --target install
+	fi
+	
+	if [[ "$3" == "iphoneos" ]] || [[ "$3" == "iphonesimulator" ]]; then
+		cmake \
+			-G "Unix Makefiles" \
+			-DCMAKE_BUILD_TYPE="${build_type}" \
+			-DIOS_ARCH="${arch}" \
+			-DPLATFORM_NAME="${platform}" \
+			-DCMAKE_INSTALL_PREFIX=${build_dir} \
+			-DCMAKE_TOOLCHAIN_FILE=../../../CMake/clang/iOS.cmake \
+			-DCMAKE_SYSTEM_NAME=iOS \
+			-DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+			-DCMAKE_PREFIX_PATH=${build_dir_root} \
+			-DPNG_STATIC=ON \
+			-DPNG_SHARED=OFF \
+			-DPNG_EXECUTABLES=OFF \
+			-DPNG_TESTS=OFF \
+			../../../${lib_name}/${lib_source_dir}
+		
+		cmake --build . --config ${build_type} --target install
+	fi
+	
+	if [[ "$3" == "android" ]]; then
+		local android_abi=
+		if [[ "$2" == "aarch64" ]]; then
+			android_abi=arm64-v8a
+		fi
+	
+		if [[ "$2" == "arm7" ]]; then
+			android_abi=armeabi-v7a
+		fi
+	
+		if [[ "$2" == "x86" ]]; then
+			android_abi=x86
+		fi
+	
+		if [[ "$2" == "x86_64" ]]; then
+			android_abi=x86_64
+		fi
+		#-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=../android-${build_type}/Conch why not work?
+		cmake -G "Unix Makefiles" \
+			-DCMAKE_INSTALL_PREFIX=${build_dir} \
+			-DCMAKE_BUILD_TYPE=${build_type} \
+			-DCMAKE_TOOLCHAIN_FILE=${CONCH_NDK_PATH}/build/cmake/android.toolchain.cmake \
+			-DANDROID_ABI=${android_abi} \
+			-DANDROID_NDK=${CONCH_NDK_PATH} \
+			-DCMAKE_ANDROID_ARCH_ABI=${android_abi} \
+			-DCMAKE_ANDROID_NDK=${CONCH_NDK_PATH} \
+			-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+			-DCMAKE_SYSTEM_NAME=Android \
+			-DCMAKE_SYSTEM_VERSION=19 \
+			-DANDROID_STL=c++_shared \
+			-DANDROID_PLATFORM=${CONCH_ANDROID_MINI_SDK_VERSION} \
+			-DANDROID_ARM_NEON=TRUE \
+			-DANDROID_TOOLCHAIN=clang \
+			-DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+			-DCMAKE_PREFIX_PATH=${build_dir_root} \
+			-DPNG_STATIC=ON \
+			-DPNG_SHARED=OFF \
+			-DPNG_EXECUTABLES=OFF \
+			-DPNG_TESTS=OFF \
+			../../../${lib_name}/${lib_source_dir}
+
+		cmake --build . --config ${build_type} --target install
+	fi
+	
+	#rm -rf ${root_dir}/${lib_name}/${lib_source_dir}
+	cd ${root_dir}
+}
 function archive_ios {
 
 	local build_type=$1
@@ -583,6 +692,21 @@ function archive_ios {
 #build_zip release "arm7" android
 #build_zip release "x86_64" android
 #build_zip release "x86" android
+
+
+#build_freetype Release "win32" windows
+#build_freetype Release "win64" windows
+
+#build_freetype release arm64 iphoneos
+#build_freetype release x86_64 iphonesimulator
+#archive_ios release iphoneos arm64 iphonesimulator x86_64
+
+#build_freetype release "aarch64" android
+#build_freetype release "arm7" android
+#build_freetype release "x86_64" android
+#build_freetype release "x86" android
+
+
 
 #build_jpeg release arm64 iphoneos
 #build_jpeg release x86_64 iphonesimulator
