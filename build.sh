e259176be2859eb718b487a42cb2a52140377cdf
xuhaodong <xuhd@layabox.com>
2024-11-13 11:38:32 +0800

    增加 libjxl 的统一编译脚本, 增加libjxl源码 注: 鸿蒙编译时通过-DCMAKE_MAKE_PROGRAM=${OHOS_NDK_CMAKE_PATH}/ninja设置自定义ninja路径; libjxl源码增加了third_party后重新repack

--- build.sh
+++ build.sh
@@ -495,6 +495,116 @@ function build_jpeg_turbo {
 	cd ${root_dir}
 }
 
+function build_jxl {
+	local build_type=$1
+  local arch=$2
+  local platform=$3
+	#depends zlib
+	#build_zlib ${build_type} ${arch} ${platform}
+	#build_openssl ${build_type} ${arch} ${platform}
+  local lib_name=libjxl
+  local build_dir_root="${root_dir}/build/${platform}-${build_type}-${arch}"
+  local build_dir="${build_dir_root}/${lib_name}"
+	mkdir -p "${build_dir}"
+	cd ${lib_name}
+	local lib_source_dir=libjxl-0.11.0
+	rm -rf ${lib_source_dir}
+	tar xvzf ${lib_source_dir}.tar.gz
+
+	cd ..
+	cd ${build_dir}
+
+	# if [[ "$3" == "windows" ]]; then
+
+	# fi
+
+  if [[ "$3" == "iphoneos" ]] || [[ "$3" == "iphonesimulator" ]]; then
+    cmake \
+      -G "Unix Makefiles" \
+      -DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+      -DCMAKE_PREFIX_PATH=${build_dir_root} \
+      -DCMAKE_BUILD_TYPE="${build_type}" \
+      -DCMAKE_TOOLCHAIN_FILE=../../../CMake/clang/iOS.cmake \
+      -DCMAKE_SYSTEM_NAME=iOS \
+      -DCMAKE_SYSTEM_PROCESSOR=${arch} \
+      -DCMAKE_C_COMPILER=/usr/bin/clang \
+      -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+      -DBUILD_TESTING=OFF \
+      -DJPEGXL_ENABLE_SJPEG=OFF \
+      -DIOS_ARCH="${arch}" \
+      -DPLATFORM_NAME="${platform}" \
+      ../../../${lib_name}/${lib_source_dir}
+
+    cmake --build . --config ${build_type} --target install
+  fi
+
+	if [[ "$3" == "android" ]]; then
+		local android_abi=
+		if [[ "$2" == "aarch64" ]]; then
+			android_abi=arm64-v8a
+		fi
+
+		if [[ "$2" == "arm7" ]]; then
+			android_abi=armeabi-v7a
+		fi
+
+		if [[ "$2" == "x86" ]]; then
+			android_abi=x86
+		fi
+
+		if [[ "$2" == "x86_64" ]]; then
+			android_abi=x86_64
+		fi
+		#-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=../android-${build_type}/Conch why not work?
+		cmake -G "Unix Makefiles" \
+			-DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+			-DCMAKE_PREFIX_PATH=${build_dir_root} \
+			-DCMAKE_BUILD_TYPE=${build_type} \
+			-DCMAKE_TOOLCHAIN_FILE=${CONCH_NDK_PATH}/build/cmake/android.toolchain.cmake \
+			-DCMAKE_ANDROID_ARCH_ABI=${android_abi} \
+			-DCMAKE_ANDROID_NDK=${CONCH_NDK_PATH} \
+			-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+			-DCMAKE_SYSTEM_NAME=Android \
+			-DCMAKE_SYSTEM_VERSION=19 \
+			-DCMAKE_CXX_FLAGS="-fno-rtti -fexceptions -Wno-multichar" \
+			-DANDROID_ABI=${android_abi} \
+      -DANDROID_NDK=${CONCH_NDK_PATH} \
+			-DANDROID_STL=c++_shared \
+			-DANDROID_PLATFORM=${CONCH_ANDROID_MINI_SDK_VERSION} \
+			-DANDROID_ARM_NEON=TRUE \
+			-DANDROID_TOOLCHAIN=clang \
+			-DBUILD_SHARED_LIBS=OFF \
+			-DBUILD_TESTING=OFF \
+			-DJPEGXL_ENABLE_SJPEG=OFF \
+			../../../${lib_name}/${lib_source_dir}
+
+    echo ${build_type}
+		cmake --build . --config ${build_type} --target install
+	fi
+
+	if [[ "$3" == "ohos" ]]; then
+    ${OHOS_NDK_CMAKE_PATH}/cmake  -G "Ninja" \
+    -DCMAKE_BUILD_TYPE=${build_type} \
+    -DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+    -DCMAKE_PREFIX_PATH=${build_dir_root} \
+    -DCMAKE_TOOLCHAIN_FILE=${OHOS_NDK_CMAKE_TOOLCHAIN_PATH} \
+    -DCMAKE_MAKE_PROGRAM=${OHOS_NDK_CMAKE_PATH}/ninja \
+    -DENABLE_STATIC=ON \
+    -DENABLE_SHARED=OFF \
+    -DOHOS_STL=c++_shared \
+    -DBUILD_TESTING=OFF \
+    -DJPEGXL_ENABLE_SJPEG=OFF \
+    -DJPEGXL_ENABLE_TESTS=OFF \
+    ../../../${lib_name}/${lib_source_dir}
+
+    #make
+    #make install
+    ${OHOS_NDK_CMAKE_PATH}/cmake --build . --config ${build_type} --target install
+  fi
+
+	rm -rf ${root_dir}/${lib_name}/${lib_source_dir}
+	cd ${root_dir}
+}
 
 function build_zip {
 	local build_type=$1
@@ -1744,6 +1854,12 @@ function clean {
 #build_zlib release "x86_64" android
 #build_zlib release "x86" android
 #build_zlib release "x86_64" linux
+
+#build_jxl release "x86_64" android
+#build_jxl release arm64 iphoneos
+#build_jxl release x86_64 iphonesimulator
+#archive_ios release iphoneos arm64 iphonesimulator x86_64
+#build_jxl release arm64 ohos
 
 #build_websocket release "x86_64" android
 #build_websocket  release "x86_64" linux
