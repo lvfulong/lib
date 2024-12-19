33017bdc7fc59c2761b0973af30c040efdf010ff
helloworldlv <775331175@qq.com>
2024-12-19 16:35:21 +0800

    build_swappy

--- build.sh
+++ build.sh
@@ -1772,6 +1772,66 @@ function build_aki {
 	#rm -rf ${root_dir}/${lib_name}/${lib_source_dir}
 	cd ${root_dir}
 }
+function build_swappy {
+	local build_type=$1
+    local arch=$2
+    local platform=$3
+	local lib_name=swappy
+	local build_dir_root="${root_dir}/build/${platform}-${build_type}-${arch}"
+    local build_dir="${build_dir_root}/${lib_name}"
+	mkdir -p "${build_dir}"
+	cd ${lib_name}
+	local lib_source_dir=gamesdk-refs_heads_main
+	rm -rf ${lib_source_dir}
+	tar xvzf ${lib_source_dir}.tar.gz
+
+	cd ..
+	cd ${build_dir}
+
+	
+	if [[ "$3" == "android" ]]; then
+		local android_abi=
+		if [[ "$2" == "aarch64" ]]; then
+			android_abi=arm64-v8a
+		fi
+	
+		if [[ "$2" == "arm7" ]]; then
+			android_abi=armeabi-v7a
+		fi
+	
+		if [[ "$2" == "x86" ]]; then
+			android_abi=x86
+		fi
+	
+		if [[ "$2" == "x86_64" ]]; then
+			android_abi=x86_64
+		fi
+		#-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=../android-${build_type}/Conch why not work?
+		cmake -G "Unix Makefiles" \
+			-DCMAKE_INSTALL_PREFIX=${build_dir_root} \
+			-DCMAKE_PREFIX_PATH=${build_dir_root} \
+			-DCMAKE_BUILD_TYPE=${build_type} \
+			-DCMAKE_TOOLCHAIN_FILE=${CONCH_NDK_PATH}/build/cmake/android.toolchain.cmake \
+			-DANDROID_ABI=${android_abi} \
+			-DANDROID_NDK=${CONCH_NDK_PATH} \
+			-DCMAKE_ANDROID_ARCH_ABI=${android_abi} \
+			-DCMAKE_ANDROID_NDK=${CONCH_NDK_PATH} \
+			-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+			-DCMAKE_SYSTEM_NAME=Android \
+			-DCMAKE_SYSTEM_VERSION=19 \
+			-DANDROID_STL=c++_shared \
+			-DANDROID_PLATFORM=${CONCH_ANDROID_MINI_SDK_VERSION} \
+			-DANDROID_ARM_NEON=TRUE \
+			-DANDROID_TOOLCHAIN=clang \
+			../../../${lib_name}/${lib_source_dir}/games-frame-pacing
+
+		cmake --build . --config ${build_type} --target install
+	fi
+
+
+	rm -rf ${root_dir}/${lib_name}/${lib_source_dir}
+	cd ${root_dir}
+}
 function archive_ios {
 
 	local build_type=$1
@@ -1818,7 +1878,7 @@ function clean {
 #build_jpeg_turbo release "arm7" android
 #build_jpeg_turbo release "x86_64" android
 #build_jpeg_turbo release "x86" android
-build_jpeg_turbo release "x86_64" linux
+#build_jpeg_turbo release "x86_64" linux
 
 #build_zip Release "win32" windows
 #build_zip Release "win64" windows
@@ -1840,7 +1900,7 @@ build_jpeg_turbo release "x86_64" linux
 #build_freetype release x86_64 iphonesimulator
 #archive_ios release iphoneos arm64 iphonesimulator x86_64
 
-#build_freetype release "aarch64" android
+build_swappy release "aarch64" android
 #build_freetype release "arm7" android
 #build_freetype release "x86_64" android
 #build_freetype release "x86" android
