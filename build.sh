7f8e96d4daad56f0506f2cd8236046c25f7ba44d
helloworldlv <775331175@qq.com>
2025-12-30 12:21:36 +0800

    降低版本

--- build.sh
+++ build.sh
@@ -933,12 +933,13 @@ function build_glslang {
     local build_dir="${build_dir_root}/${lib_name}"
 	mkdir -p "${build_dir}"
 	cd ${lib_name}
-	local lib_source_dir=glslang-16.1.0
+	local lib_source_dir=glslang-15.4.0
 	rm -rf ${lib_source_dir}
 	tar xvzf ${lib_source_dir}.tar.gz
 
 	cd ${lib_source_dir} 
-	python ./update_glslang_sources.py
+	#python ./update_glslang_sources.py
+	python3 ./update_glslang_sources.py
 	cd ..
 
 	cd ..
