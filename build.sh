41da7bf96ac0d507e74ec733615a6c78a64624fa
layabox <platform1@layabox.com>
2026-05-12 20:49:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir -p "${ios_fat}"
 
 
 #export ANDROID_HOME=E:/github/lib2/android-ndk-r25c-windows/android-ndk-r25c
-export ANDROID_HOME=/Applications/AndroidNDK13676358.app/Contents/NDK
+export ANDROID_HOME=/Users/layabox/Library/Android/sdk/ndk/28.2.13676358
 
 #CONCH_NDK_VERSION=21.0.6113669
 CONCH_NDK_PATH=${ANDROID_HOME}
